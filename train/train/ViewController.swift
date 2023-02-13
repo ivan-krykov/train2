1c16dcfa7ee33428b8a88814ca85ea9937ca5c2f
Ivan Krykov <ivankrykov@icloud.com>
2023-02-13 13:12:54 +0300

    initional

---
+++ train/train/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  train
+//
+//  Created by Иван Крыков on 2/13/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
